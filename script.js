82d0849ca81fc7dc61d9ec5aee618fada829706f
ellisvelandia <eyis619@gmail.com>
2022-10-27 13:43:15 -0500

    elements

--- script.js
+++ script.js
@@ -1,8 +1,47 @@
 const getCurrencyOptions = async () => {
   const response = await fetch("https://api.exchangerate.host/symbols");
-  const json = response.json();
-  console.log(response);
+  const json = await response.json();
+
   return json.symbols;
 };
 
-getCurrencyOptions();
+const getCurrencyRate = async (fromCurrency, toCurrency) => {
+  const currencyCovertUrl = new URL("https://api.exchangerate.host/convert");
+
+  currencyCovertUrl.searchParams.append("from", fromCurrency);
+  currencyCovertUrl.searchParams.append("to", toCurrency);
+
+  const response = await fetch(currencyCovertUrl);
+  const json = await response.json();
+
+  return json.result;
+};
+
+const appendOptionToSelect = (selectElement, optionItem) => {
+  const optionElement = document.createElement("option");
+  optionElement.value = optionItem.code;
+  optionElement.textContent = optionItem.description;
+
+  selectElement.appendChild(optionElement);
+};
+
+const populateSelectElement = (selectElement, optionList) => {
+  optionList.forEach((optionItem) => {
+    appendOptionToSelect(selectElement, optionItem);
+  });
+};
+
+const setupCurrencies = async () => {
+  const fromCurrencyElem = document.getElementById("fromCurrency");
+  const toCurrencyElem = document.getElementById("toCurrency");
+
+  const currencyOptions = await getCurrencyOptions();
+
+  const currencies = Object.keys(currencyOptions).map(
+    (currencyKey) => currencyOptions[currencyKey]
+  );
+
+  console.log(currencies);
+};
+
+setupCurrencies();
